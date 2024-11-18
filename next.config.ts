f6dba2f5440cbcc3bfaf3565202b1375ecd34e26
JoshuaAAX <joshua.chicame@correounivalle.edu.co>
2024-11-17 23:17:10 -0500

    change root page

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   async redirects() {
     return [
-     
-      
+      {
+        source: '/',
+        destination: '/capitulos/introduccion', // La ruta que quieres como principal
+        permanent: true, // true = código 308, false = código 307
+      },
       
     ]
   },
